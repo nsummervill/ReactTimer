d37ad6265c4cebc0ea60bb57196ec4c4dc095a24
Nicholas Summervill <nsummervill@yahoo.com>
2018-06-22 21:43:35 -0500

    Initial Commit

---
+++ app/components/Nav.jsx
@@ -0,0 +1,31 @@
+var React = require('react');
+var {Link, IndexLink} = require('react-router');
+
+var Nav = React.createClass({
+  render: function() {
+    return(
+      <div className="top-bar">
+        <div className="top-bar-left">
+          <ul className="menu">
+            <li className="menu-text">React Timer</li>
+            <li>
+              <IndexLink to="/" activeClassName="active-link" activeStyle={{fontWeight: 'bold'}}>Timer</IndexLink>
+            </li>
+            <li>
+              <Link to="/about" activeClassName="active-link" activeStyle={{fontWeight: 'bold'}}>Countdown</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="top-bar-right menu-text">
+          <li>
+          Created by Nick Summervill
+          </li>
+
+        </div>
+      </div>
+
+    );
+  }
+});
+
+module.exports = Nav;
